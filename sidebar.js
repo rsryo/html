fadb02e21ff2b02ea7d0de680f32a015bb442102
rsryo <rs.ryoichi.09@gmail.com>
2023-02-17 18:30:11 +0900

    プライベートポリシー画面完成

--- sidebar.js
+++ sidebar.js
@@ -1,15 +1,11 @@
-$(function(){
-    var duration = 300;
-    var aside = $(".page_main > aside");
-    var asideButton = aside.find("button");
-    asideButton.on("click", function(){
-        aside.toggleClass("open");
-        if(aside.hasClass("open")){
-            aside.stop(true).animate({left: "-70px"}, duration);
-            //asideButton.find("img").attr("src", "images/btn_close.png");
-        }else{
-            aside.stop(true).animate({left: "-350px"}, duration);
-            //asideButton.find("img").attr("src", "images/btn_open.png");
-        };
-    });
- });
+let nav= document.querySelector("#navArea");
+let btn= document.querySelector(".toggle-btn");
+let mask= document.querySelector("#mask");
+
+btn.onclick = () => {
+    nav.classList.toggle("open");
+};
+
+mask.onclick = () => {
+    nav.classList.toggle("open");
+}
